dddc69fea7b1ebcbf982900cc9c81eb0c82d25b0
Tristan Brown <tgbrown@stanford.edu>
2018-07-31 00:09:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,3 @@
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}  "
   puts "-----------"
@@ -7,7 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-it 'prints a blank board when the board array is empty ' do
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-it 'prints a board with o in the top left' do
-  board = ["o", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board
